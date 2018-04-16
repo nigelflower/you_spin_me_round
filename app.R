9dadab63d46868ed30287116d6d8e452f0e4c017
Jason Guo <jguo28@uic.edu>
2018-04-16 14:40:00 -0500

    Organized  Graph but didnt sort yet

--- app.R
+++ app.R
@@ -1,5 +1,36 @@
+library(scales)
 library(shiny)
 library(shinydashboard)
+library(ggplot2)
+library(lubridate)
+library(DT)
+library(jpeg)
+library(grid)
+library(leaflet)
+
+# Dev Tool for GitHub
+library(devtools)
+library(plotly)
+library(dplyr)
+library(stringr)
+library(reshape2)
+library(plyr)
+
+setwd("/Users/jason/Documents/GitHub/you_spin_me_round")
+tornadoes <- read.csv("tornadoes.csv")
+
+IL_Code <- 17
+
+# Get IL tornadoes from file              
+illinois_tornadoes <- subset(tornadoes, stf == IL_Code)
+
+#combine all the tornadoes from the f1-f4 code counts excluding the 0th county
+illinois_counties <- as.data.frame(table(a = c(illinois_tornadoes[,"f1"], illinois_tornadoes[,"f2"], illinois_tornadoes[,"f3"], illinois_tornadoes[,"f4"])))
+illinois_counties <- illinois_counties[-c(1), ]
+
+names(illinois_counties) <- c("County", "Frequency")
+
+
 
 ui <- dashboardPage(
     dashboardHeader(title = "CS 424 Project 3: You Spin me Round", titleWidth = "100%"),
@@ -23,8 +54,17 @@ ui <- dashboardPage(
                     h4(style = "font-size: 150%",a(href = "https://www.evl.uic.edu/aej/424/", "Course website"))
             ),
             
-            tabItem(tabName="Tornadoes"
-            
+            tabItem(tabName="Tornadoes",
+                    fluidRow(
+                      title = "Tornado County", solidHeader = TRUE, status = "primary", width = 5,
+                             id = "countyTable",
+                             dataTableOutput("countyTable")
+                      ),
+                    fluidRow(
+                      title = "Tornado County", solidHeader = TRUE, status = "primary", width = 5,
+                             id = "countyChart",
+                             plotOutput("countyChart")
+                    )
                     
             ),
             
@@ -41,7 +81,17 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output, session){
-        
+  output$countyTable <- renderDataTable({
+    datatable(illinois_counties, 
+              options = list(searching = FALSE, pageLength = 6, lengthChange = FALSE))
+  })
+  
+
+  output$countyChart <- renderPlot({
+    ggplot(illinois_counties, aes(x=illinois_counties$County, y=illinois_counties$Frequency))  +
+      geom_bar(position="dodge", stat="identity", fill = "orange") + labs(x="County Number", y = "# of Tornadoes") + theme(axis.text.x = element_text(angle = 70, vjust=0.5))
+  })
+  
 }
 
 shinyApp(ui, server)
